248c37883f86f47d4e7c9e53e30ac4c7660e3fce
hefeng6500 <2443992009@qq.com>
2020-06-07 15:02:27 +0800

    annotate: 添加配置文件的注释

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
-import buble from '@rollup/plugin-buble'
-import replace from '@rollup/plugin-replace'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+import buble from '@rollup/plugin-buble'   // 编译 es6+ 以兼容 IE11
+import replace from '@rollup/plugin-replace' // 可在打包是替换文件中的字符串
+import resolve from '@rollup/plugin-node-resolve' // 实现 node 中 require.resolve() 中的功能， require 模块
+import commonjs from '@rollup/plugin-commonjs' //可将CommonJS模块转换为ES6
+import { terser } from 'rollup-plugin-terser' // 压缩插件
 import pkg from './package.json'
 
+// 引入 package.json 获取 插件版本 version 字段值，在打包的文件首行添加版本信息，日期，作者信息。
 const banner = `/*!
  * vuex v${pkg.version}
  * (c) ${new Date().getFullYear()} Evan You
  * @license MIT
  */`
 
+// 多入口创建
 export function createEntries(configs) {
   return configs.map((c) => createEntry(c))
 }
@@ -35,12 +37,14 @@ function createEntry(config) {
     c.output.name = c.output.name || 'Vuex'
   }
 
+  // 这里就用到了 @rollup/plugin-replace 插件， 将字符串 '__VERSION__' 转化为 package.json 中配置 version加载打包后文件的头部
   c.plugins.push(replace({
     __VERSION__: pkg.version,
     __DEV__: config.format !== 'umd' && !config.browser
       ? `(process.env.NODE_ENV !== 'production')`
       : config.env !== 'production'
   }))
+
 
   if (config.transpile !== false) {
     c.plugins.push(buble())
--- rollup.main.config.js
+++ rollup.main.config.js
@@ -1,5 +1,6 @@
 import { createEntries } from './rollup.config'
 
+// createEntries 函数用于创建入口数组，最后导出的是一个包含多个入口的一个数组
 export default createEntries([
   { input: 'src/index.js', file: 'dist/vuex.esm.browser.js', format: 'es', browser: true, transpile: false, env: 'development' },
   { input: 'src/index.js', file: 'dist/vuex.esm.browser.min.js', format: 'es', browser: true, transpile: false, minify: true, env: 'production' },
